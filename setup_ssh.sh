f116b89c0b30f2ab300a26e53b225e2573562a90
Igor Candido <igorcr39@gmail.com>
2024-02-01 06:44:36 -0300

    changes

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Cria a pasta .ssh se ela não existir
-mkdir -p ~/.ssh
+# Caminho completo para o diretório .ssh
+ssh_dir="$HOME/.ssh"
 
-cd .ssh/
+# Cria o diretório .ssh se ele não existir
+mkdir "$ssh_dir"
 
-# Extrai a chave privada do secrets do GitHub Actions
-echo "${PRIVATE_KEY}" > ~/.ssh/id_rsa
+# Extrai a chave privada do secrets do GitHub Actions e salva no arquivo id_rsa dentro de .ssh
+echo "$PRIVATE_KEY" > "$ssh_dir/id_rsa"
 
 # Ajusta as permissões do arquivo da chave privada
-chmod 600 ~/.ssh/id_rsa
+chmod 600 "$ssh_dir/id_rsa"
 
 # Adiciona a chave ao agente SSH para permitir o acesso aos repositórios
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+ssh-add "$ssh_dir/id_rsa"
